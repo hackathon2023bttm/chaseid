e781ac8523882c65de52f5494bf56d4b0c82bb50
ivyhu630 <ajianouc@gmail.com>
2023-06-15 12:58:48 -0700

    add model for opsProfile

---
+++ src/app/lib/models/OperationProfile.ts
@@ -0,0 +1,23 @@
+import { Schema, model, models } from 'mongoose'
+
+const OperationProfileSchema = new Schema({
+    dba_name: {
+    type: String,
+  },
+  description: {
+    type: String,
+  },
+  naics: {
+    type: String
+  }
+},
+ { timestamps: {
+    createdAt: 'created_at',
+    updatedAt: 'updated_at',
+  }}
+);
+
+
+const opsProfileModel = models.operation_profile || model('operation_profile', OperationProfileSchema);
+
+export default opsProfileModel;
