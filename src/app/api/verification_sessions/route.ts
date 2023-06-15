a98820d5947660585621723b39570b7b0499d820
Matthew Tso <matthewt@wepay.com>
2023-06-15 10:08:32 -0700

    Add verification session

---
+++ src/app/api/verification_sessions/route.ts
@@ -0,0 +1,30 @@
+import dbConnect from '@/app/lib/mongodb'
+import { NextRequest, NextResponse } from 'next/server'
+import VerificationSession from '@/app/lib/models/VerificationSession'
+import { pick } from 'lodash';
+
+const port = process.env.PORT || 3000
+const base = process.env.NODE_ENV === 'development' ? ('http://localhost:' + port) : 'https://chaseid.fly.dev'
+
+export async function POST(request: NextRequest) {
+
+  await dbConnect()
+
+  try {
+    const json = await request.json();
+    const parsed = pick(json, ['custom_data', 'flow_redirect_url', 'profiles'])
+    const verifSession = await VerificationSession.create(parsed)
+    const verificationSessionUrl = base + '/verify/' + verifSession._id
+    // const custom_data = json.custom_data
+    console.log({ json });
+
+    const response = Object.assign({}, {
+      verification_session_url: verificationSessionUrl,
+    }, verifSession.toObject());
+
+    return NextResponse.json(response)
+  } catch (e) {
+    console.error('Failed to create verification session', e);
+    return NextResponse.json({ error: 'internal_error' }, { status: 500 })
+  }
+}
---
+++ src/app/lib/models/VerificationSession.ts
@@ -0,0 +1,35 @@
+import { Schema, model, models } from 'mongoose'
+
+const ProfileSchema = new Schema({
+  requested: {
+    type: Boolean,
+  },
+  type: {
+    type: String,
+  },
+});
+
+const VerificationSessionSchema = new Schema({
+  custom_data: {
+    type: String,
+  },
+  flow_redirect_url: {
+    type: String,
+  },
+  profiles: {
+    type: [ProfileSchema],
+  },
+  app_id: {
+    type: Schema.Types.ObjectId,
+    // ref: 'app'
+  },
+}, {
+  timestamps: {
+    createdAt: 'created_at',
+    updatedAt: 'updated_at',
+  },
+});
+
+const sessionModel = models.verification_session || model('verification_session', VerificationSessionSchema);
+
+export default sessionModel;
